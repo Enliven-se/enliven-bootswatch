1802fcb531dd51959e934fa0477737db3c9055d2
klokie <github@klokie.com>
2016-08-09 22:09:07 +0200

    fixed JS & Bower build, removed Modernizr

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+$ = jQuery = require('../../bower_components/jquery/dist/jquery.min.js');
+var bootstrap = require('../../bower_components//bootstrap-sass/assets/javascripts/bootstrap.js');
+
+require('./custom.js');
