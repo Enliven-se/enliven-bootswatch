ead503e05c060395f3d74f6c391a251e6c37fa94
klokie <github@klokie.com>
2016-08-09 19:23:46 +0200

    added Sass, Compass, Gulp styleguide support

---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+// generated on 2016-06-08 using generator-webapp 2.1.0
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const $ = gulpLoadPlugins();
+
+const fs = require('fs');
+const browserSync = require('browser-sync');
+const del = require('del');
+const wiredep = require('wiredep').stream;
+const browserify = require('browserify');
+const babelify = require('babelify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+
+const reload = browserSync.reload;
+
+const destination = 'guide';
+
+gulp.task('styles', () => {
+  return gulp.src('*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('html', ['styles'], () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({
+      searchPath: ['.tmp', 'app', '.']
+    }))
+    .pipe($.if('*.css', $.cssnano({
+      safe: true,
+      autoprefixer: false
+    })))
+    .pipe($.if('*.html', $.htmlmin({
+      collapseWhitespace: true
+    })))
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest(destination));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', destination]));
+
+gulp.task('serve', ['styles'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/*.html'
+  ]).on('change', reload);
+
+  gulp.watch('*.scss', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: [destination]
+    }
+  });
+});
+
+gulp.task('serve:test', [], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+});
+
+// inject bower components
+gulp.task('wiredep', () => {
+  gulp.src('*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap-sass'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['html', 'extras'], () => {
+  return gulp.src(destination + '/**/*').pipe($.size({
+    title: 'build',
+    gzip: true
+  }));
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
+});
