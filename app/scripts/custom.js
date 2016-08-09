450b5e35673040f33a8d4865bc7e007871ea97b7
klokie <github@klokie.com>
2016-08-09 22:10:10 +0200

    reformat JS

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -10,21 +10,21 @@
         $('#home > .navbar').addClass('navbar-transparent');
   });
 
-  $("a[href='#']").click(function(e) {
+  $('a[href=\'#\']').click(function(e) {
     e.preventDefault();
   });
 
-  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+  var $button = $('<div id=\'source-button\' class=\'btn btn-primary btn-xs\'>&lt; &gt;</div>').click(function(){
     var html = $(this).parent().html();
     html = cleanSource(html);
-    $("#source-modal pre").text(html);
-    $("#source-modal").modal();
+    $('#source-modal pre').text(html);
+    $('#source-modal').modal();
   });
 
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
 
-  $(".bs-component").hover(function(){
+  $('.bs-component').hover(function(){
     $(this).append($button);
     $button.show();
   }, function(){
@@ -32,11 +32,11 @@
   });
 
   function cleanSource(html) {
-    html = html.replace(/×/g, "&times;")
-               .replace(/«/g, "&laquo;")
-               .replace(/»/g, "&raquo;")
-               .replace(/←/g, "&larr;")
-               .replace(/→/g, "&rarr;");
+    html = html.replace(/×/g, '&times;')
+               .replace(/«/g, '&laquo;')
+               .replace(/»/g, '&raquo;')
+               .replace(/←/g, '&larr;')
+               .replace(/→/g, '&rarr;');
 
     var lines = html.split(/\n/);
 
@@ -44,7 +44,7 @@
     lines.splice(-1, 1);
 
     var indentSize = lines[0].length - lines[0].trim().length,
-        re = new RegExp(" {" + indentSize + "}");
+        re = new RegExp(' {' + indentSize + '}');
 
     lines = lines.map(function(line){
       if (line.match(re)) {
@@ -54,7 +54,7 @@
       return line;
     });
 
-    lines = lines.join("\n");
+    lines = lines.join('\n');
 
     return lines;
   }
