38b3229fb84d98670827464f7687cb557533ab03
klokie <github@klokie.com>
2018-03-15 14:51:20 +0100

    version bump => 0.8.0

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: "enliven:enliven-bootswatch",
-  summary: "ENLIVEN basic styles package",
-  version: "0.7.0",
-  git: "https://gitlab.com/enliven/enliven-bootswatch.git"
+  name: 'enliven:enliven-bootswatch',
+  summary: 'ENLIVEN basic styles package',
+  version: '0.8.0',
+  git: 'https://gitlab.com/enliven/enliven-bootswatch.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(["METEOR@1.0"]);
+  api.versionsFrom(['METEOR@1.0']);
 
-  api.use(["vulcan:core@1.3.2", "fourseven:scss@4.5.0"]);
+  api.use(['vulcan:core', 'fourseven:scss@4.5.4']);
 
-  api.addFiles(["app/styles/client.scss"], ["client"]);
+  api.addFiles(['app/styles/client.scss'], ['client']);
 
   // api.addAssets([
   //   'app/images/logo.png',
